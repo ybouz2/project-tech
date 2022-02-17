685a85f4d247635d1fa9601c77274d04bafee9ba
ybouz2 <yousri.bouzaien@hva.nl>
2022-02-17 17:18:19 +0100

    Handlebars

--- server/app.js
+++ server/app.js
@@ -1,29 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use (express.static('public'))
+const { engine } = require ('express-handlebars');
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', engine({
+  layoutsDir: `${__dirname}/views/layouts`,
+  extname: 'hbs',
+  defaultLayout: 'index',
+  partialsDir: `${__dirname}/views/partials`
+  }));
+
+app.use (express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Dit is een test voor de opdracht serve!')
+  res.render('main');
 })
 
 app.get('/login', (req, res) => {
-  res.send("Log In of maak een account")
-})
+  res.send("Log In of maak een account");
+});
 
 app.get('/about', (req, res) => {
-  res.send("Over mij")
-})
+  res.send("Over mij");
+});
 
 app.get('/matches', (req, res) => {
-  res.send("Hier kun je je matches zien")
-})
+  res.send("Hier kun je je matches zien");
+});
 
 app.get('*', (req, res) => {
-  res.send("404 page")
-})
+  res.send("404 page");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
