8d79567819ff7aa60eadcf986a94a993b7c08658
ybouz2 <yousri.bouzaien@hva.nl>
2022-03-10 15:26:11 +0100

    database connect

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config()
 
 
@@ -9,32 +8,30 @@ const flash = require('express-flash')
 const session = require('express-session')
 const initialize = require('./')
 const methodOverride = require('method-override')
-
 const app = express();
 const port = 3000;
 const { engine } = require ('express-handlebars');
-
+const {saveUser} = require("./utils/db");
 const users = [];
 
-const {utilsDB}  = require('./utils/db')
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.weqjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('connection made')
-  // perform actions on the collection object
-  client.close();
-});
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.weqjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+  
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 const initializePassport = require('./passport-config')
 
 initializePassport(
   passport, 
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
+ 
+    email => saveUser(userMeta).find(user => user.email === email),
+   id => saveUser(userMeta).find(user => user.id === id)
+ )
 
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: false}))
@@ -67,7 +64,7 @@ app.engine('hbs', engine({
 app.use (express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('main', {name: req.user.name});
+  res.render('main') //{name: req.user.name});
 })
 
 app.get('/login',  (req, res) => {
@@ -87,16 +84,29 @@ app.get('/register', (req, res) => {
   res.render("register");
 });
 
+
+
 app.post('/register',  async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-    users.push({
+    const userMeta = {
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword
-    })
+    };
+
+    console.log('testen')
+    saveUser(userMeta);
+
+    // users.push({
+    //   id: Date.now().toString(),
+    //   name: req.body.name,
+    //   email: req.body.email,
+    //   password: hashedPassword
+    // })
+
+
     res.redirect('/login')
 
   } catch {
@@ -105,6 +115,8 @@ app.post('/register',  async (req, res) => {
   
   console.log(users)
 })
+
+
 
 app.get('*', (req, res) => {
   res.send("404 page");
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,16 +1,44 @@
-exports.utilsDB = async function (client) {
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.weqjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ client.connect(err => {
+ const collection = client.db("test").collection("devices");
+  
+ //perform actions on the collection object
+ client.close();
+ });
+
+
+
+
+const saveUser = async function (user) {
+console.log('test')
     try {
-        await client.connect()
-
-        const database = client.db('userdb')
-
-        const collection = database.collection('users')
-
-        // Stopt de data uit mijn database in een Array
-        return collection.find().toArray() 
-
-    } catch (err) {
-        console.log(err)
+  
+      await client.connect();
+        console.log('connected')
+      const result = await client
+  
+        .db("Accounts")
+  
+        .collection("AllAccounts")
+  
+        .insertOne(user);
+  
+      console.log("Account opgeslagen met id: " + result.insertedId);
+  
+    } catch (e) {
+  
+      console.error(e);
+  
+    } finally {
+  
+      await client.close();
+  
     }
+  
+  };
 
-}
+  module.exports = {saveUser}
