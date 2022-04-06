3083e93a8e4ba2d4e25d3533fac8f0b05a7bf43e
ybouz2 <yousri.bouzaien@hva.nl>
2022-04-06 23:20:56 +0200

    Added API

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,26 @@
+const resDiv = document.querySelector('#results');
+
+ 
+window.onload = () => {
+  getAdvice();
+};
+ 
+ 
+ 
+function getAdvice() {
+ 
+  fetch('https://api.adviceslip.com/advice').then(response => {
+    return response.json();
+  }).then(adviceData => {
+    const Adviceobj = adviceData.slip;
+    resDiv.innerHTML = `<p>"${Adviceobj.advice}"</p>`;
+  }).catch(error => {
+    console.log(error);
+  });
+ 
+}
+
+
 const terms = document.getElementById("terms");
 const btn = document.getElementById("submit");
 
@@ -16,4 +39,10 @@ terms.onchange = function(){
 	}
 };
 
+
+ 
+
+ 
+ 
+ 
 
